6e9606fd3f4ad66a2b86ea0da31de503b6664e28
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-12-13 08:34:02 -0300

    component hierarchy

---
+++ 02_projeto_vue_cli/src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+    <form action="">
+        <input type="text">
+        <input type="text">
+        <input type="submit" value="Enviar">
+    </form>    
+</template>
+
+<script>
+    export default {
+        name: 'Form'
+    }
+</script>
---
+++ 02_projeto_vue_cli/src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Está é a descrição da pessoa: {{ nome }}</h2>
+        <Info />
+    </div>
+</template>
+
+<script>
+    import Info from './Info'
+
+    export default {
+        name: 'Pessoa',
+        components: {
+            Info
+        },
+        data() {
+            return {
+                nome: 'Luis'
+            }
+        }
+    }
+</script>
