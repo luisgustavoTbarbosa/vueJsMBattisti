89b3dba1385b0654598a2f9984b91d853889e182
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-12-13 07:56:27 -0300

    life cycle created and mounted

---
+++ 02_projeto_vue_cli/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'Ainda não sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Luís Gustavo'
+            }, 1000)
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'João'
+            }, 2000)
+        }
+    }
+</script>
